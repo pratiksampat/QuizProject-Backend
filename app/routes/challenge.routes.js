92a554e3ca75db5cb31a715cf4a9ec551e3c8650
Rishabh Dutt <rishabh97dutt@gmail.com>
2018-11-22 19:46:22 +0530

    Server-Sent-Event

--- app/routes/challenge.routes.js
+++ app/routes/challenge.routes.js
@@ -9,4 +9,5 @@ module.exports = (app) => {
     app.post('/challenge/getQuestions',challengeController.getQuestions); // Get challenge questions
     app.get('/challenge/FindChallenge',challengeController.FindChallenge); // Find a challenger
     app.post('/challenge',challengeController.challenge); // sse a challenger
+    app.get('/stream', challengeController.sse);
 }
